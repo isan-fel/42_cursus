d5bdb028a05a75f2a4ad8a34350d94828a76293e
isan-fel <sanfe.24@gmail.com>
2021-03-30 19:20:00 +0200

    strdup_working

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,17 +6,16 @@
 /*   By: isan-fel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 13:31:08 by isan-fel          #+#    #+#             */
-/*   Updated: 2021/03/17 14:12:36 by isan-fel         ###   ########.fr       */
+/*   Updated: 2021/03/30 19:19:11 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
 
-int		ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
 	int n;
-	
+
 	n = 0;
 	while (str[n] != '\0')
 	{
@@ -27,13 +26,13 @@ int		ft_strlen(char *str)
 
 char	*ft_strdup(char *src)
 {
-	char *result;
-	int src_len;
-	int i;
+	char	*result;
+	int	src_len;
+	int	i;
 
+	i = 0;
 	src_len = ft_strlen(src);
-	result = (char *) malloc(sizeof(char)*src_len);
-	//*src = *result;
+	result = (char *)malloc(sizeof(char) * src_len + 1);
 	while (src[i] != '\0')
 	{
 		result[i] = src[i];
@@ -41,13 +40,4 @@ char	*ft_strdup(char *src)
 	}
 	result[i] = '\0';
 	return (result);
-}
-
-int main ()
-{
-	char *source = "hola";
-	//char *result;
-	
-	//result = ft_strdup(source);
-	printf("%s\n", ft_strdup(source));
 }
