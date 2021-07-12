c0bea01fac3ec90a397247bc5225a199fb352cb2
Ivan San felipe <isan-fel@c1r10s2.42madrid.com>
2021-07-12 13:44:36 +0200

    change_how_count_2

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: isan-fel <isan-fel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 11:41:10 by isan-fel          #+#    #+#             */
-/*   Updated: 2021/07/12 13:14:33 by isan-fel         ###   ########.fr       */
+/*   Updated: 2021/07/12 13:42:24 by isan-fel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,6 @@ char	*ft_save_char_array_arg(va_list param, char str, st_flags *flags)
 		flags->arg = temp;
 		ft_write_char(flags);
 	}
-	//printf("count:%d\n", flags->len);
 	if(str == 's')
 	{
 		temp = ft_strdup_string_printf(va_arg(param, char *));
@@ -80,16 +79,6 @@ char	*ft_save_hexa_pointer_arg(va_list param, char str, st_flags *flags)
 	return(temp);
 }
 
-int ft_int_negative(char str, st_flags *flags)
-{
-	//if (str == '-' && (flags->width || flags->prec))
-	if (str == '-')
-		return (-1);
-	if (str == '0' && flags->dot)
-		return (0);
-	return (1);
-}
-
 char	*ft_save_int_arg(va_list param, char str, st_flags *flags)
 {
 	char *temp;
@@ -103,52 +92,19 @@ char	*ft_save_int_arg(va_list param, char str, st_flags *flags)
 	return (temp);
 }
 
-int	ft_type_arg(va_list param, char str, st_flags *flags)
+void	ft_type_arg(va_list param, char str, st_flags *flags)
 {
 	char *temp;
-	int len = 0;
-	int negative = 1;
 	
 	flags->type = str;
 	//printf("type:%c\n", str);
 	if(str == 'c' || str == 's' || str == '%')
 		temp = ft_save_char_array_arg(param, str, flags);
 	if(str == 'd' || str == 'i' || str == 'u')
-	{
 		temp = ft_save_int_arg(param, str, flags);
-		negative = ft_int_negative(temp[0], flags);
-	}
 	if(str == 'x' || str == 'X' || str == 'p')
 		temp = ft_save_hexa_pointer_arg(param, str, flags);
-	//printf("temp:%s", temp);
-	len = ft_strlen(temp);
 	free(temp);
-	return (len * negative);
-}
-
-int	ft_continue_count(st_flags flags, int len, int count)
-{
-	/*if (len < 0 && flags.width <= flags.prec && flags.width && flags.prec)
-		return (count + ft_count_arglen(flags, len * -1) + 1);
-	if (len < 0)	
-		return (count + ft_count_arglen(flags, len * -1));
-	else
-		return (count + ft_count_arglen(flags, len));*/
-	if (len < 0)
-	{
-		len = len * -1;
-		//printf("func:%d", ft_count_arglen(flags, len));
-		if (flags.width <= flags.prec && ((flags.width && flags.prec) || flags.dot))
-		{
-			if (len < flags.width || len < flags.prec)
-				return (count + ft_count_arglen(flags, len) + 1);
-			else
-				return (count + ft_count_arglen(flags, len));
-		}
-		else
-			return (count + ft_count_arglen(flags, len));
-	}
-	return (count + ft_count_arglen(flags, len));
 }
 
 int	ft_printf(const char *str, ...)
@@ -156,10 +112,7 @@ int	ft_printf(const char *str, ...)
 	va_list	param;
 	st_flags flags;
 	int n;
-	//int count;
-	int len;
 
-	//count = 0;
 	n = 0;
 	va_start(param, str);
 	flags.len = 0;
@@ -170,15 +123,11 @@ int	ft_printf(const char *str, ...)
 		{
 			++n;
 			n = ft_save_every_flag(param, str, n, &flags);
-			len = ft_type_arg(param, str[n++], &flags);
-			//count = ft_continue_count(flags, len, count)
-			//printf("suma:%d + %d\n", count, flags.len);
+			ft_type_arg(param, str[n++], &flags);
 		}
 		else
 			flags.len += write(1, &str[n++], 1);
-	//printf("suma:%d + %d\n", count, flags.len);
 	}
-	//printf("suma:%d + %d\n", count, flags.len);
 	va_end(param);
 	return (flags.len);
 }
--- printf/libftprintf.h
+++ printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: isan-fel <isan-fel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 14:14:17 by isan-fel          #+#    #+#             */
-/*   Updated: 2021/07/12 13:17:51 by isan-fel         ###   ########.fr       */
+/*   Updated: 2021/07/12 13:42:37 by isan-fel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,6 @@ int			ft_save_every_flag(va_list param, const char *str, int n, st_flags *flags)
 void		ft_write_int(st_flags *flags);
 void    	ft_int_prec_or_len(st_flags *flags, char *num, int intlen);
 int			ft_write_char(st_flags *flags);
-int 		ft_count_arglen(st_flags flags, int len);
 void		ft_write_string(st_flags *flags);
 char		*ft_uint_itoa(unsigned int n);
 char 		*ft_write_hex(unsigned int n, st_flags flags);
