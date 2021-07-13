0987682f4d7f0995b5c9500d0d08a0afc8a93355
Ivan San felipe <isan-fel@c1r10s2.42madrid.com>
2021-07-13 16:09:57 +0200

    add_bonus

---
+++ printf/bonus/ft_save_flags_2_bonus.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_save_flags_2.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isan-fel <isan-fel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/06 16:43:33 by isan-fel          #+#    #+#             */
+/*   Updated: 2021/07/13 16:04:06 by isan-fel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libftprintf_bonus.h"
+
+int	ft_save_width(const char *str, t_flags *flags, int n)
+{
+	ft_width(str, n, flags);
+	return (n + ft_intlen(flags->width));
+}
+
+int	ft_dot_logic(va_list param, const char *str, t_flags *flags, int n)
+{
+	while (str[n] != '.')
+	{
+		if (str[n] == '*')
+		{
+			n = ft_asterik(param, str, n, flags);
+			break ;
+		}
+		if (ft_isdigit(str[n]))
+		{
+			n = ft_save_width(str, flags, n);
+			break ;
+		}
+		++n;
+	}
+	while (!ft_isalpha(str[++n]))
+	{
+		if (str[n] == '*')
+		{
+			n = ft_asterik(param, str, n, flags);
+			break ;
+		}
+		if (ft_isdigit(str[n]))
+			return (n + ft_prec(str, n, flags));
+	}
+	return (n);
+}
+
+int	ft_zero_or_justify(t_flags *flags, char str)
+{
+	if (str == '+' || str == '-')
+		return (ft_justify(str, flags));
+	if (str == '0')
+		return (ft_zero(str, flags));
+	return (0);
+}
+
+int	save_every_flag(va_list param, const char *str, int n, t_flags *flags)
+{
+	int	save_n;
+
+	while (str[n] == '+' || str[n] == '-' || str[n] == '0')
+		 n += ft_zero_or_justify(flags, str[n]);
+	save_n = n;
+	while (!ft_isalpha(str[n]) && str[n] != '%')
+	{
+		if (str[n] == '.')
+		{	
+			flags->dot = 1;
+			return (ft_dot_logic(param, str, flags, save_n));
+		}
+		++n;
+	}
+	n = save_n;
+	if (ft_isdigit(str[n]))
+		n = ft_save_width(str, flags, n);
+	if (str[n] == '*')
+		n = ft_asterik(param, str, n, flags);
+	while (!ft_strchr(FLAGS, str[n]) && str[n] != '\0')
+		if (str[n++] == ' ')
+			flags->prec += write(1, &str[n], 1);
+	return (n);
+}
---
+++ printf/bonus/ft_uint_itoa_bonus.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_uint_itoa.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isan-fel <isan-fel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/09 21:08:41 by user42            #+#    #+#             */
+/*   Updated: 2021/07/13 16:04:06 by isan-fel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libftprintf_bonus.h"
+
+static int	ft_intlonglen(long nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb == 0)
+		len = 1;
+	while (nb >= 1)
+	{
+		nb = nb / 10;
+		++len;
+	}
+	return (len);
+}
+
+static char	*ft_write(char *str, int numlen, long nb, int flag_neg)
+{
+	str[numlen] = '\0';
+	--numlen;
+	while (numlen > 0)
+	{
+		if (nb < 9)
+			str[numlen] = ((nb % 10) + 48);
+		else
+		{
+			str[numlen] = ((nb % 10) + 48);
+			nb = nb / 10;
+		}
+		--numlen;
+	}
+	if (flag_neg == 0)
+		str[numlen] = ((nb % 10) + 48);
+	return (str);
+}
+
+char	*ft_uint_itoa(unsigned int n)
+{
+	int		numlen;
+	char	*str;
+	long	nc;
+
+	if (n < 0)
+	{
+		nc = n * -1;
+		numlen = ft_intlonglen(nc) + 1;
+		str = (char *)malloc(sizeof(char) * numlen + 1);
+		if (!str)
+			return (NULL);
+		str[0] = 45;
+		ft_write(str, numlen, nc, 1);
+	}
+	else
+	{
+		nc = n;
+		numlen = ft_intlonglen(nc);
+		str = (char *)malloc(sizeof(char) * numlen + 1);
+		if (!str)
+			return (NULL);
+		ft_write(str, numlen, nc, 0);
+	}
+	return (str);
+}
